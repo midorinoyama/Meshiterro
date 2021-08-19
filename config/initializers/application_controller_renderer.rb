0a4b0669f90115ff1c6bfd1455a9f0b66989e91e
midorinoyama <fujiaqa@gmail.com>
2021-08-19 07:05:34 +0000

    [FINISH]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a4d0b627d5bf43a588d5deeb452261398f7a8595d2e4aace5b4c69827415b8b7d4e29a9df0f6f8be47fca96efcac9e72ab31d2becd913abd009be8832d295dfc'
